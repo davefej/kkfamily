1c65f72f960b858c60c0d86dad0a13a73f5db553
davefej <davefej@gmail.com>
2017-02-23 00:09:34 +0100

    printed

--- helper/insert.php
+++ helper/insert.php
@@ -25,7 +25,7 @@ if(array_key_exists("type",$data)){
 			if($data["type"] == "trash"){
 				$sql = "INSERT INTO `trash` (`id`, `pallet_id`, `amount`, `time`, `user_id`, `deleted`) VALUES (NULL, '".$data['pallet_id']."', '".$data['amount']."', CURRENT_TIMESTAMP, '".$_SESSION['user_id']."', '0')";
 			}else if($data["type"] == "pallet"){
-				$sql = "INSERT INTO `pallet` (`id`, `product_id`, `supplier_id`, `time`, `amount`, `user_id`, `deleted`) VALUES (NULL, '".$data['product_id']."', '".$data['supplier_id']."', CURRENT_TIMESTAMP, '".$data['amount']."', '".$_SESSION['user_id']."', '0')";
+				$sql = "INSERT INTO `pallet` (`id`, `product_id`, `supplier_id`, `time`, `amount`, `printed`, `user_id`, `deleted`) VALUES (NULL, '".$data['product_id']."', '".$data['supplier_id']."', CURRENT_TIMESTAMP, '".$data['amount']."', '0', '".$_SESSION['user_id']."', '0')";
 			}else if($data["type"] == "output"){
 				$sql = "INSERT INTO `output` (`id`, `pallet_id`, `amount`, `time`, `user_id`, `deleted`) VALUES (NULL, '".$data['pallet_id']."', '".$data['amount']."', CURRENT_TIMESTAMP, '".$_SESSION['user_id']."', '0')";
 			}else if($data["type"] == "alert"){
