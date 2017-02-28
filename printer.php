1185ff1afe0b6b7adb6ada2c69f77639352c7838
davefej <davefej@gmail.com>
2017-02-28 09:19:43 +0100

    printer php

---
+++ printer.php
@@ -0,0 +1,41 @@
+<?php
+require 'helper/mysqli.php';
+
+if(isset($_GET['pass']) && $_GET['pass'] == "kkpass123"){
+	
+	if(isset($_GET['error'])){		
+		$myfile = fopen("printerror.txt", "a");
+		fwrite($myfile, "\r\n". date('Y-m-d h:i:sa'). "\r\n");
+		fwrite($myfile, $_GET['error']);
+		fclose($myfile);
+		return;
+	}
+	$id = "";
+	$str = "NO_DATA";
+	$mysqli = connect();
+	if($results = $mysqli->query("SELECT p.id as id, p.amount as amount,pr.name as prod, s.name as sup, p.time as time, pr.unit as unit from pallet p,product pr, supplier s WHERE p.deleted = 0 and p.printed = 0 and p.product_id = pr.id and p.supplier_id = s.id")){		
+		while($row = $results->fetch_assoc()) {
+			$str = $row['id']."#_#".$row['amount']."#_#".$row['prod']."#_#".$row['time']."#_#".$row['sup']."#_#".$row['unit']."#_#";
+			$id = $row['id'];
+			break;
+		}		
+		$results->free();
+	}else{
+		$str = "ERROR";
+	}
+	if($str != "NO_DATA" && str != "ERROR" && $id != "" && !isset($_GET['test'])){
+		
+		update("UPDATE pallet SET printed = '1' WHERE id = '".$id."'");
+	}
+	// close connection
+	$mysqli->close();
+	
+	
+	print $str;
+	
+	
+}
+
+
+
+?>
