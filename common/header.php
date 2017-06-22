7f28eed5885d59ebd001ae3d28f0696f0c750995
davefej <davefej@gmail.com>
2017-06-22 13:40:55 +0200

    bug fix

--- common/header.php
+++ common/header.php
@@ -31,21 +31,21 @@
 		<meta charset="utf-8">
 		
 		<script src="../js/print.min.js" type="text/javascript" ></script>
-		<script src="../js/functions.js?v=33" ></script>
-		<script src="../js/server.js?v=33" ></script>
+		<script src="../js/functions.js?v=34" ></script>
+		<script src="../js/server.js?v=34" ></script>
 		<script src="../js/Chart.js"></script>
 		<script src="../js/sorttable.js"></script>
 
 		
 	<?php  if($selected == "tablet"){	//TODO css rand		?>							
 				<link href="../js/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
-				<script src="../js/tablet.js?v=33"></script>
+				<script src="../js/tablet.js?v=34"></script>
 				<link rel="stylesheet" type="text/css" href="../css/tabletstyles.css?v=3">
 	<?php }else{ ?>
 				
 				<link rel="stylesheet" type="text/css" href="../css/print.min.css">
 				<link href="../js/bootstrap-3.3.7-dist/css/bootstrap.css?v=10" rel="stylesheet">
-				<link rel="stylesheet" type="text/css" href="../css/styles.css?v=10">
+				<link rel="stylesheet" type="text/css" href="../css/styles.css?v=11">
 	<?php } ?>
 <?php 		
 	if(isset($_SESSION['theme']) && $_SESSION['theme'] != "0"){	
--- tablet/dailyoutput.php
+++ tablet/dailyoutput.php
@@ -6,7 +6,8 @@ require("../common/header.php");
 
 $to = date("Y-m-d")." 23:59:59";
 $from = date('Y-m-d', strtotime('-2 days'))." 00:00:01";
-
+echo "<br/>";
+echo "<br/>";
 dailyOutputSqlTablet($from,$to,true);
 ?>
 
